ecc399fcf26aeb1bb97d6bf8b331bd4dcabcd55a
Subhash <kakisubhash007@gmail.com>
2023-12-26 22:24:16 +0530

    added button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
+import {useState} from 'react';
 
 function App() {
+
+  const [state, setState] = useState('');
+
   return (
     <div className="App">
     <h1>Best Places To Celebrate New Year</h1>
     <img src='./images/newyear.jpg'></img>
+         <h2>{state}</h2>
+    <button onClick={()=>{
+      setState('Advance Happy New year');
+    }}>Click Me</button>
     </div>
   );
 }

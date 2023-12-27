6f8812da0effee12b5db43dfce4e793e2c769f8f
anjum <ow.um1718@gmail.com>
2023-12-27 11:14:16 +0530

    Anjum added background-color in app.css and a div with className=anjum in app.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,16 @@ function App() {
   return (
     <div className="App">
     <h1>Best Places To Celebrate New Year</h1>
-    <img src='./images/newyear.jpg'></img>
+    <img src='./images/newyear.jpg' alt=""></img>
          <h2>{state}</h2>
     <button onClick={()=>{
       setState('Advance Happy New year');
     }}>Click Me</button>
+
+    <div className="anjum">
+      <h1>Happy winter to all by <span>Anjum</span></h1>
+    </div>
+    
     </div>
   );
 }
